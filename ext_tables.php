a1640b8285803fbd00d9e1f78189b8e81f1fba77
fazzyx <fazzyx@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-12 10:42:49 +0000

    Version 0.2 Add Dam, Dam Cat and Directory support as image selection mode Make configuration of single selection aka Manual mode easier
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cfa_mooflow/trunk@11891 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -11,7 +11,11 @@ t3lib_extMgm::addPlugin(array('LLL:EXT:cfa_mooflow/locallang_db.xml:tt_content.l
 t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","MooFlow V0.2 integration");
 
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] ='pi_flexform';
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY . '/flexform_ds.xml');
+if (t3lib_extMgm::isLoaded('dam')) {
+  t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY . '/flexformDAM_ds.xml');
+} else { 
+  t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY . '/flexform_ds.xml');
+}
 
 if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_cfamooflow_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_cfamooflow_pi1_wizicon.php';
 ?>
