771421cfbd29fc0a36d2d11ae2f46bc075890aa2
fazzyx <fazzyx@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-07 22:21:19 +0000

    Bugfix #1973 : target of internal link use '_blank' as default. Can now overwrite by configuration
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cfa_mooflow/trunk@13922 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "cfa_mooflow"
 #
-# Auto generated 30-10-2008 21:21
+# Auto generated 07-11-2008 23:09
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.3.0',
+	'version' => '0.3.1',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'dam_catedit' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:47:{s:9:"ChangeLog";s:4:"ee27";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"98ef";s:17:"ext_localconf.php";s:4:"49bc";s:14:"ext_tables.php";s:4:"f6c0";s:18:"flexformDAM_ds.xml";s:4:"655f";s:15:"flexform_ds.xml";s:4:"efbe";s:13:"locallang.xml";s:4:"391d";s:16:"locallang_db.xml";s:4:"bb23";s:14:"doc/manual.sxw";s:4:"938e";s:19:"doc/wizard_form.dat";s:4:"77bc";s:20:"doc/wizard_form.html";s:4:"182a";s:22:"pi1/FlexformConfig.php";s:4:"5462";s:14:"pi1/ce_wiz.gif";s:4:"98ef";s:31:"pi1/class.tx_cfamooflow_pi1.php";s:4:"8ddb";s:39:"pi1/class.tx_cfamooflow_pi1_wizicon.php";s:4:"c93c";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/dynLoader.php";s:4:"6f22";s:17:"pi1/locallang.xml";s:4:"7375";s:24:"pi1/static/editorcfg.txt";s:4:"fe9e";s:18:"res/MooFlow.Mod.js";s:4:"485d";s:15:"res/MooFlow.css";s:4:"f9c7";s:14:"res/MooFlow.js";s:4:"792c";s:24:"res/mootools-1.2-core.js";s:4:"c492";s:24:"res/mootools-1.2-more.js";s:4:"6775";s:24:"res/skin/ajax_loader.gif";s:4:"fc44";s:24:"res/skin/left-slider.gif";s:4:"31b7";s:17:"res/skin/left.gif";s:4:"28c9";s:26:"res/skin/middle-slider.gif";s:4:"0aa9";s:19:"res/skin/middle.gif";s:4:"bed2";s:17:"res/skin/play.gif";s:4:"ba4f";s:19:"res/skin/resize.gif";s:4:"4d4a";s:18:"res/skin/right.gif";s:4:"2f8a";s:17:"res/skin/stop.gif";s:4:"ef2c";s:21:"res/ReMooz/ReMooz.css";s:4:"2c96";s:20:"res/ReMooz/ReMooz.js";s:4:"e213";s:23:"res/ReMooz/closebox.gif";s:4:"1574";s:23:"res/ReMooz/closebox.png";s:4:"f2aa";s:24:"res/ReMooz/remo_bg_e.png";s:4:"bb7d";s:24:"res/ReMooz/remo_bg_n.png";s:4:"b80c";s:25:"res/ReMooz/remo_bg_ne.png";s:4:"a631";s:25:"res/ReMooz/remo_bg_nw.png";s:4:"b03b";s:24:"res/ReMooz/remo_bg_s.png";s:4:"1d8a";s:25:"res/ReMooz/remo_bg_se.png";s:4:"6ec7";s:25:"res/ReMooz/remo_bg_sw.png";s:4:"df78";s:24:"res/ReMooz/remo_bg_w.png";s:4:"dbc5";s:22:"res/ReMooz/spinner.gif";s:4:"0483";}',
+	'_md5_values_when_last_written' => 'a:47:{s:9:"ChangeLog";s:4:"ee27";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"98ef";s:17:"ext_localconf.php";s:4:"49bc";s:14:"ext_tables.php";s:4:"f6c0";s:18:"flexformDAM_ds.xml";s:4:"655f";s:15:"flexform_ds.xml";s:4:"efbe";s:13:"locallang.xml";s:4:"391d";s:16:"locallang_db.xml";s:4:"2501";s:14:"doc/manual.sxw";s:4:"6b48";s:19:"doc/wizard_form.dat";s:4:"77bc";s:20:"doc/wizard_form.html";s:4:"182a";s:22:"pi1/FlexformConfig.php";s:4:"5462";s:14:"pi1/ce_wiz.gif";s:4:"98ef";s:31:"pi1/class.tx_cfamooflow_pi1.php";s:4:"455c";s:39:"pi1/class.tx_cfamooflow_pi1_wizicon.php";s:4:"c93c";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/dynLoader.php";s:4:"6f22";s:17:"pi1/locallang.xml";s:4:"7375";s:24:"pi1/static/editorcfg.txt";s:4:"fe9e";s:18:"res/MooFlow.Mod.js";s:4:"485d";s:15:"res/MooFlow.css";s:4:"f9c7";s:14:"res/MooFlow.js";s:4:"792c";s:24:"res/mootools-1.2-core.js";s:4:"c492";s:24:"res/mootools-1.2-more.js";s:4:"6775";s:24:"res/skin/ajax_loader.gif";s:4:"fc44";s:24:"res/skin/left-slider.gif";s:4:"31b7";s:17:"res/skin/left.gif";s:4:"28c9";s:26:"res/skin/middle-slider.gif";s:4:"0aa9";s:19:"res/skin/middle.gif";s:4:"bed2";s:17:"res/skin/play.gif";s:4:"ba4f";s:19:"res/skin/resize.gif";s:4:"4d4a";s:18:"res/skin/right.gif";s:4:"2f8a";s:17:"res/skin/stop.gif";s:4:"ef2c";s:21:"res/ReMooz/ReMooz.css";s:4:"2c96";s:20:"res/ReMooz/ReMooz.js";s:4:"e213";s:23:"res/ReMooz/closebox.gif";s:4:"1574";s:23:"res/ReMooz/closebox.png";s:4:"f2aa";s:24:"res/ReMooz/remo_bg_e.png";s:4:"bb7d";s:24:"res/ReMooz/remo_bg_n.png";s:4:"b80c";s:25:"res/ReMooz/remo_bg_ne.png";s:4:"a631";s:25:"res/ReMooz/remo_bg_nw.png";s:4:"b03b";s:24:"res/ReMooz/remo_bg_s.png";s:4:"1d8a";s:25:"res/ReMooz/remo_bg_se.png";s:4:"6ec7";s:25:"res/ReMooz/remo_bg_sw.png";s:4:"df78";s:24:"res/ReMooz/remo_bg_w.png";s:4:"dbc5";s:22:"res/ReMooz/spinner.gif";s:4:"0483";}',
 	'suggests' => array(
 	),
 );
